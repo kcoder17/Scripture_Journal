1326e4e65cb4668f77058cdc499d385759581da8
Brad <bradkellogg7@gmail.com>
2019-03-05 03:16:19 -0700

    Added pagination feature

--- ScriptureJournal/Pages/JournalEntries/Index.cshtml.cs
+++ ScriptureJournal/Pages/JournalEntries/Index.cshtml.cs
@@ -19,15 +19,34 @@ namespace ScriptureJournal.Pages.JournalEntries
             _context = context;
         }
 
-        public IList<JournalEntry> JournalEntry { get;set; }
+        public string BookSort { get; set; }
+        public string DateSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
+        public PaginatedList<JournalEntry> JournalEntry { get;set; }
         [BindProperty(SupportsGet = true)]
         public string SearchString { get; set; }
         public SelectList Books { get; set; }
         [BindProperty(SupportsGet = true)]
         public string Book { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortOrder, string currentFilter, string searchString, int? pageIndex)
         {
+            CurrentSort = sortOrder;
+            BookSort = String.IsNullOrEmpty(sortOrder) ? "book_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            CurrentFilter = searchString;
+
             // Use LINQ to get list of books.
             IQueryable<string> bookQuery = from m in _context.JournalEntry
                                             orderby m.Book
@@ -45,8 +64,24 @@ namespace ScriptureJournal.Pages.JournalEntries
                 entries = entries.Where(x => x.Book == Book);
             }
 
+            switch (sortOrder)
+            {
+                case "book_desc":
+                    entries = entries.OrderByDescending(s => s.Book);
+                    break;
+                case "Date":
+                    entries = entries.OrderBy(s => s.Date);
+                    break;
+                case "date_desc":
+                    entries = entries.OrderByDescending(s => s.Date);
+                    break;
+                default:
+                    entries = entries.OrderBy(s => s.Book);
+                    break;
+            }
+            int pageSize = 5;
             Books = new SelectList(await bookQuery.Distinct().ToListAsync());
-            JournalEntry = await entries.ToListAsync();
+            JournalEntry = await PaginatedList<JournalEntry>.CreateAsync(entries.AsNoTracking(), pageIndex ?? 1, pageSize);
         }
     }
 }
